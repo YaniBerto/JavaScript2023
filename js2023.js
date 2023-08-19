fb5db33bc75cbfbfc291cb8392cfef8dae290c3a
unknown <yanina.bertolotti@gmail.com>
2023-08-19 16:37:41 -0300

    preEntrega1Bertolotti

--- js2023.js
+++ js2023.js
@@ -1,3 +1,6 @@
+let ingreso = prompt("quiere entrar SI o NO");
+while (ingreso!="NO"){
+
 let cantidadArticulos = prompt("Ingrese cantidad de articulos");
 
 if (cantidadArticulos > 0){
@@ -8,8 +11,8 @@ else{
   console.log("0 Articulos")
 }
 
-let metros = prompt("ingrese cant. metros") 
-const precio = 3000 
+let metros = prompt("ingrese cant. metros"); 
+const precio = 3000;
 
 function calcular_total(){
 
@@ -20,13 +23,5 @@ return calcular_total;
 }
 
 calcular_total();
-//objeto literal
-let auto_uno = {
-  color: "blanco",
-  marca: "Peugeot",
-  Modelo: "208"
 
 }
-console.log(auto_uno);
-console.log("Datos del auto uno")
-console.log("color:", auto_uno.color)
